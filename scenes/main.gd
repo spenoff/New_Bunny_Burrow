b6876b37c10b0b5acbeeffff1c73c408ef0e713e
Spencer R Nisonoff <snisonoff@gmail.com>
2024-02-24 16:59:24 -0800

    pause menu following camera

---
+++ scenes/main.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var pause_menu = $CanvasLayer/PauseMenu
+var paused = false
+
+func _ready():
+	pause_menu.hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+	
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		
+	paused = !paused
